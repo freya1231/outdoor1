183d296038345b7b49103848636018ebe3ba5d5c
freya1231 <tara.bartnik@gmail.com>
2023-06-18 22:36:07 +0100

    updating to online links

--- header.js
+++ header.js
@@ -3,59 +3,59 @@ let appHeader = `
       <nav>
       <div class="menu-area">
     <ul>
-        <li><a href="/index.html">Home</a></li>
-        <li><a href="/activities.html">Activities</a>
+        <li><a href="https://freya1231.github.io/outdoor1//index.html">Home</a></li>
+        <li><a href="https://freya1231.github.io/outdoor1//activities.html">Activities</a>
             <ul class="dropdown-1">
-                <li><a href="/activities/hike.html">Walks and hiking</a>
+                <li><a href="https://freya1231.github.io/outdoor1//activities/hike.html">Walks and hiking</a>
                         <ul class="dropdown-2">
-                            <li><a href="/Users/tarabartnik/Documents/coding/repos/outdoor1/activities/hike/day-hikes.html">Day-trip walks and hikes</a></li>
-                            <li><a href="/Users/tarabartnik/Documents/coding/repos/outdoor1/activities/hike/overnight-hikes.html">Overnight hikes</a></li>
-                            <li><a href="/Users/tarabartnik/Documents/coding/repos/outdoor1/activities/hike/London-walks.html">London based walks</a></li>
+                            <li><a href="/https://freya1231.github.io/outdoor1/activities/hike/day-hikes.html">Day-trip walks and hikes</a></li>
+                            <li><a href="https://freya1231.github.io/outdoor1/activities/hike/overnight-hikes.html">Overnight hikes</a></li>
+                            <li><a href="https://freya1231.github.io/outdoor1/outdoor1/activities/hike/London-walks.html">London based walks</a></li>
                         </ul>
                 </li>
-                <li><a href="/Users/tarabartnik/Documents/coding/repos/outdoor1/activities/cycle.html">Cycling</a>
+                <li><a href="https://freya1231.github.io/outdoor1/activities/cycle.html">Cycling</a>
                         <ul class="dropdown-2">
-                            <li><a href="/Users/tarabartnik/Documents/coding/repos/outdoor1/activities/cycle/road-biking.html">Road biking</a></li>
-                            <li><a href="/Users/tarabartnik/Documents/coding/repos/outdoor1/activities/cycle/gravel-biking.html">Gravel biking</a></li>
-                            <li><a href="/Users/tarabartnik/Documents/coding/repos/outdoor1/activities/cycle/mountain-biking.html">Mountain biking</a></li>
+                            <li><a href="https://freya1231.github.io/outdoor1/activities/cycle/road-biking.html">Road biking</a></li>
+                            <li><a href="https://freya1231.github.io/outdoor1/activities/cycle/gravel-biking.html">Gravel biking</a></li>
+                            <li><a href="https://freya1231.github.io/outdoor1/activities/cycle/mountain-biking.html">Mountain biking</a></li>
                             <li><a href="#">Bike-packing trips</a></li>
-                            <li><a href="/Users/tarabartnik/Documents/coding/repos/outdoor1/activities/cycle/cycling-gear.html">Gear advice</a></li>
+                            <li><a href="https://freya1231.github.io/outdoor1/activities/cycle/cycling-gear.html">Gear advice</a></li>
                         </ul>
                 </li>
-                <li><a href="/Users/tarabartnik/Documents/coding/repos/outdoor1/activities/camp.html">Camping</a>
+                <li><a href="https://freya1231.github.io/outdoor1/activities/camp.html">Camping</a>
                         <ul class="dropdown-2">
-                            <li><a href="/Users/tarabartnik/Documents/coding/repos/outdoor1/activities/camp/coastal-camping.html">Beachside</a></li>
-                            <li><a href="/Users/tarabartnik/Documents/coding/repos/outdoor1/activities/camp/forest-camping.html">Forest</a></li>
-                            <li><a href="/Users/tarabartnik/Documents/coding/repos/outdoor1/activities/camp/wild-camping.html">Wild camping</a></li>
-                            <li><a href="/Users/tarabartnik/Documents/coding/repos/outdoor1/activities/camp/glamping.html">Glamping</a></li>
+                            <li><a href="https://freya1231.github.io/outdoor1/activities/camp/coastal-camping.html">Beachside</a></li>
+                            <li><a href="https://freya1231.github.io/outdoor1/activities/camp/forest-camping.html">Forest</a></li>
+                            <li><a href="https://freya1231.github.io/outdoor1/activities/camp/wild-camping.html">Wild camping</a></li>
+                            <li><a href="https://freya1231.github.io/outdoor1/activities/camp/glamping.html">Glamping</a></li>
                         </ul>
                 </li>
                 
-                <li><a href="/Users/tarabartnik/Documents/coding/repos/outdoor1/activities/water.html">Water activities</a>
+                <li><a href="https://freya1231.github.io/outdoor1/activities/water.html">Water activities</a>
                         <ul class="dropdown-2">
-                            <li><a href="/Users/tarabartnik/Documents/coding/repos/outdoor1/activities/water/London-swimming.html">Swim</a></li>
-                            <li><a href="/Users/tarabartnik/Documents/coding/repos/outdoor1/activities/water/kayak-canoe.html">Kayak & canoe</a></li>
+                            <li><a href="https://freya1231.github.io/outdoor1/activities/water/London-swimming.html">Swim</a></li>
+                            <li><a href="https://freya1231.github.io/outdoor1/activities/water/kayak-canoe.html">Kayak & canoe</a></li>
                             <li><a href="#">Sailing</a></li>
                         </ul>
                 </li>
                 
-                <li><a href="/Users/tarabartnik/Documents/coding/repos/outdoor1/activities/nature-stays.html">Nature stays</a>
+                <li><a href="https://freya1231.github.io/outdoor1/nature-stays.html">Nature stays</a>
                         <ul class="dropdown-2">
-                            <li><a href="/Users/tarabartnik/Documents/coding/repos/outdoor1/activities/stay/x1.html">Shepherd's huts</a></li>
-                            <li><a href="/Users/tarabartnik/Documents/coding/repos/outdoor1/activities/stay/x2.html">Cottages</a></li>
-                            <li><a href="/Users/tarabartnik/Documents/coding/repos/outdoor1/activities/stay/x3.html">Lodges</a></li>
-                            <li><a href="/Users/tarabartnik/Documents/coding/repos/outdoor1/activities/stay/x4.html">Festivals</a></li>
+                            <li><a href="https://freya1231.github.io/outdoor1/activities/stay/x1.html">Shepherd's huts</a></li>
+                            <li><a href="https://freya1231.github.io/outdoor1/activities/stay/x2.html">Cottages</a></li>
+                            <li><a href="https://freya1231.github.io/outdoor1/activities/stay/x3.html">Lodges</a></li>
+                            <li><a href="https://freya1231.github.io/outdoor1/activities/stay/x4.html">Festivals</a></li>
                         </ul>
                 </li>
                 
-                <li><a href="/Users/tarabartnik/Documents/coding/repos/outdoor1/activities/wildlife.html">Wildlife watching</a>
+                <li><a href="https://freya1231.github.io/outdoor1/activities/wildlife.html">Wildlife watching</a>
                         <ul class="dropdown-2">
                             <li><a href="#">Flora</a></li>
                             <li><a href="#">Fauna</a></li>
                         </ul>
                 </li>
                 
-                <li><a href="/Users/tarabartnik/Documents/coding/repos/outdoor1/activities/x.html">Wellness</a>
+                <li><a href="https://freya1231.github.io/outdoor1/activities/x.html">Wellness</a>
                         <ul class="dropdown-2">
                             <li><a href="#">Yoga</a></li>
                             <li><a href="#">Breathwork</a></li>
@@ -65,7 +65,7 @@ let appHeader = `
         </li>
         <li><a href="#">Blog</a></li>
         <li><a href="#">Gear</a></li>
-        <li><a href="/Users/tarabartnik/Documents/coding/repos/outdoor1/about.html">About</a></li>
+        <li><a href="https://freya1231.github.io/outdoor1/about.html">About</a></li>
         <li><a href="#">Contact</a></li>
     </ul>
 </nav>
